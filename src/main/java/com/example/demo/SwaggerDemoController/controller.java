5287aa4686349ebf561deadf0fdc850276c45c1e
zhukaixin <798130749@qq.com>
2019-04-16 04:10:44 +0800

    2

--- src/main/java/com/example/demo/SwaggerDemoController/controller.java
+++ src/main/java/com/example/demo/SwaggerDemoController/controller.java
@@ -2,8 +2,8 @@ package com.example.demo.SwaggerDemoController;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class controller {
 
     @ApiOperation(value = "测试swagger", notes = "测试swagger")
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping(value = "/")
     public String getStudent() {
 
         return "success";
